76d0b6d2932bbca2c697780c9b9c5f0d39eb3c3c
Gabriella Braz <gabibraz15@outlook.com>
2024-09-03 14:36:46 -0300

    [ADD] ICONS BOOTSTRAP TOOLTIP

--- app/src/utils/radar_chart.py
+++ app/src/utils/radar_chart.py
@@ -206,3 +206,49 @@ def render_radar_chart(
 
     # RENDERIZA O RADAR CHART
     st_echarts(radar_chart, key=f"radar_chart_{cd_ponto}_{radar_chart_column}")
+
+
+dict_cores = {
+    "AMARELO": """<div style="display: flex; align-items: center; padding-right: 5px;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="yellow" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
+                    </svg>
+                </div>""",
+    "VERDE": """<div style="display: flex; align-items: center; padding-right: 5px;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="green" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </svg>
+                </div>""",
+    "VERMELHO": """<div style="display: flex; align-items: center; padding-right: 5px;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="red" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+                    </svg>
+                </div>""",
+    None: """<div style="display: flex; align-items: center; padding-right: 5px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="grey" class="bi bi-circle-fill" viewBox="0 0 16 16">
+                <circle cx="8" cy="8" r="8"/>
+                </svg>
+            </div>""",
+}
+
+
+# Função para criar o HTML
+def generate_html(data_dict):
+    html_output = ""
+    for category, sub_dict in data_dict.items():
+        # Começa o bloco de cada categoria
+        html_output += f"""<div style="font-size: 15px; font-weight: bold; margin-bottom: 5px;">{category}</div>"""
+
+        for key, value in sub_dict.items():
+            valor = float(value["VALOR"])
+            farol = value["FAROL"]
+            if valor != 0:  # Apenas adiciona ao HTML se o valor for diferente de 0
+                # Monta o HTML para cada item com base no valor do farol
+                html_output += f"""<div style="display: flex; align-items: center; margin-bottom: -18px;">
+                                    {dict_cores[farol]}
+                                    <div style="font-size: 13px;">
+                                        {key}: <b style="color">{valor}</b>
+                                    </div>
+                                </div><br/>"""
+
+    return html_output
